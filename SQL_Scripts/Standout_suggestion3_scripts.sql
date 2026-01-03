ceca3fed165da749f2bf3b81c5ff99647e016cca
its-developer-hub <getitrajesh@rediffmail.com>
2026-01-03 19:03:40 +1100

    Rename user from NoMgr to NonMgr in SQL script

--- SQL_Scripts/Standout_suggestion3_scripts.sql
+++ SQL_Scripts/Standout_suggestion3_scripts.sql
@@ -1,13 +1,13 @@
 /*-----------------
-Create a non-management user named NoMgr. 
+Create a non-management user named NonMgr. 
 Show the code of how your would grant access to the database(hr_db), 
 but revoke access to the salary data.
 -------------------*/
 
-CREATE USER NoMgr WITH PASSWORD 'ABC@123';
+CREATE USER NonMgr WITH PASSWORD 'ABC@123';
 
-GRANT CONNECT ON DATABASE hr_db TO NoMgr;
-GRANT USAGE ON SCHEMA hr_schema TO NoMgr;
-GRANT SELECT ON ALL TABLES IN SCHEMA hr_schema TO NoMgr;
+GRANT CONNECT ON DATABASE hr_db TO NonMgr;
+GRANT USAGE ON SCHEMA hr_schema TO NonMgr;
+GRANT SELECT ON ALL TABLES IN SCHEMA hr_schema TO NonMgr;
 
-REVOKE SELECT ON hr_schema.salary FROM NoMgr;
+REVOKE SELECT ON hr_schema.salary FROM NonMgr;
